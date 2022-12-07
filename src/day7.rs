8309e9c27c86a89cdb430c29f2bec4957b5ab381
Zachary Dremann <dremann@gmail.com>
2022-12-07 10:46:57 -0500

    We only need a list of directory sizes

--- src/day7.rs
+++ src/day7.rs
@@ -1,85 +1,44 @@
-use std::collections::HashMap;
-use std::ops::ControlFlow;
+pub fn generator(s: &str) -> Vec<u32> {
+    let mut results = Vec::with_capacity(1024);
+    let mut in_progress = Vec::with_capacity(64);
 
-#[derive(Debug, Default)]
-pub struct Directory<'a> {
-    sub_dirs: HashMap<&'a str, Box<Directory<'a>>>,
-    total_size: u32,
-}
+    // Add a root directory
+    in_progress.push(0);
 
-fn populate_dir<'a>(dir: &mut Directory<'a>, lines: &mut impl Iterator<Item = &'a str>) {
-    while let Some(line) = lines.next() {
-        if line == "$ cd .." {
-            return;
-        } else if line == "$ ls" {
-            // Do nothing
-        } else if let Some(dst) = line.strip_prefix("$ cd ") {
-            let subdir = dir.sub_dirs.get_mut(dst).unwrap();
-            let prev_subsize = subdir.total_size;
-            populate_dir(subdir, lines);
-            let new_subsize = subdir.total_size;
-            dir.total_size += new_subsize - prev_subsize;
-        } else if let Some(dir_name) = line.strip_prefix("dir ") {
-            dir.sub_dirs
-                .insert(dir_name, Box::new(Directory::default()));
+    for line in s.lines() {
+        if line == "$ cd /" || line == "$ ls" || line.starts_with("dir ") {
+            continue;
+        } else if line == "$ cd .." {
+            results.push(in_progress.pop().unwrap());
+        } else if line.starts_with("$ cd ") {
+            in_progress.push(0u32);
         } else {
             let (size, _name) = line.split_once(' ').unwrap();
             let size: u32 = size.parse().unwrap();
-            dir.total_size += size;
+            in_progress.iter_mut().for_each(|d| *d += size);
         }
     }
+    results.extend_from_slice(&in_progress);
+    results.sort_unstable();
+    results
 }
 
-pub fn generator(s: &str) -> Directory {
-    let mut root = Directory::default();
-
-    let mut lines = s.lines();
-    assert_eq!(lines.next().unwrap(), "$ cd /");
-
-    populate_dir(&mut root, &mut lines);
-
-    root
+pub fn part_1(sizes: &[u32]) -> u32 {
+    let idx = sizes.partition_point(|&size| size < 100_000);
+    sizes[..idx].iter().sum()
 }
 
-pub fn visit_smaller_dirs(d: &Directory, visit: &mut impl FnMut(&Directory) -> ControlFlow<()>) {
-    if visit(d).is_break() {
-        return;
-    }
-    for sub_dir in d.sub_dirs.values() {
-        visit_smaller_dirs(sub_dir, visit);
-    }
-}
-
-pub fn part_1(d: &Directory) -> u32 {
-    let mut total_size = 0;
-    visit_smaller_dirs(d, &mut |d| {
-        if d.total_size <= 100_000 {
-            total_size += d.total_size
-        }
-        ControlFlow::Continue(())
-    });
-    total_size
-}
-
-pub fn part_2(d: &Directory) -> u32 {
+pub fn part_2(sizes: &[u32]) -> u32 {
     const DISK_SIZE: u32 = 70_000_000;
     const NEEDED_SPACE: u32 = 30_000_000;
 
-    let available_space = DISK_SIZE - d.total_size;
+    // Last item will be the root directory, since it has the largest size
+    let available_space = DISK_SIZE - sizes.last().unwrap();
     let required_to_free = NEEDED_SPACE - available_space;
-    let mut best_space = u32::MAX;
 
-    visit_smaller_dirs(d, &mut |d| {
-        if d.total_size < required_to_free {
-            return ControlFlow::Break(());
-        }
-
-        best_space = best_space.min(d.total_size);
-
-        ControlFlow::Continue(())
-    });
-
-    best_space
+    // will point to the first item which fails the predicate
+    let idx = sizes.partition_point(|&size| size < required_to_free);
+    sizes[idx]
 }
 
 super::day_test! {demo_1 == 95437}
