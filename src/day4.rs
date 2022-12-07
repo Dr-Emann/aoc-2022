d0eb9b4d4b481c5ae945b169afb5fdb82a557684
Zachary Dremann <dremann@gmail.com>
2022-12-07 11:51:27 -0500

    Speed up day 4 a little

--- src/day4.rs
+++ src/day4.rs
@@ -1,19 +1,18 @@
+use std::str::FromStr;
+
 type Range = (u32, u32);
 type Pair = (Range, Range);
-
-fn parse_range(s: &str) -> Range {
-    let (begin, end) = s.split_once('-').unwrap();
-    let begin: u32 = begin.parse().unwrap();
-    let end: u32 = end.parse().unwrap();
-
-    (begin, end)
-}
 
 pub fn generator(s: &str) -> Vec<Pair> {
     let mut result = Vec::with_capacity(1024);
     s.lines().for_each(|line| {
         let (l, r) = line.split_once(',').unwrap();
-        result.push((parse_range(l), parse_range(r)))
+        let mut items = line.split([',', '-']).map(|s| u32::from_str(s).unwrap());
+
+        result.push((
+            (items.next().unwrap(), items.next().unwrap()),
+            (items.next().unwrap(), items.next().unwrap()),
+        ))
     });
     result
 }
